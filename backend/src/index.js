0e510162c682ee99392cfc59478fedf9be9419a2
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2025-10-11 13:05:32 -0700

    Downgrade to express 4.21.2 and have the webserver serve the frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { createServer } from "http";
 import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Serve frontend build files
+app.use(express.static(path.resolve(__dirname, '../../frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../frontend/dist/index.html'));
+});
 
 io.on("connection", (socket) => {
   console.log("New player connected:", socket.id);
