dcbdcb58f2e6eee258b0adebb97ffc05c542e3f3
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2025-10-11 17:43:03 -0700

    Update dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import MiniGame from './pages/MiniGame';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Router>
@@ -18,7 +18,7 @@ function App() {
         <Route
           path="/"
           element={
-            <>
+            <div>
               <div>
                 <a href="https://vite.dev" target="_blank">
                   <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -46,12 +46,12 @@ function App() {
               <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
               </p>
-            </>
+            </div>
           }
         />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
