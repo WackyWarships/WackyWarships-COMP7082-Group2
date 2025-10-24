5107defdb7d0e17682303f0aff638cb1e2ceab6e
Daniel Lau <daniellau200@gmail.com>
2025-10-24 08:09:09 -0700

    Updated CreateLobby.ts to emit CreateLobby event to backend and adjusted lobby.ts to account for changes to lobby related types

--- backend/src/lobby.ts
+++ backend/src/lobby.ts
@@ -21,76 +21,103 @@ const playerToLobbyIdMap = new Map<PlayerId, LobbyId>();
 
 export function setupSocket(io: Server<ClientToServerEvents, ServerToClientEvents>, socket: Socket) {
     socket.on('createLobby', (payload: CreateLobbyEvent) => {
+        const { hostId, hostName, lobbyName, settings } = payload;
         const lobbyId = crypto.randomUUID();
         socket.join(lobbyId);
-        playerToLobbyIdMap.set(payload.playerId, lobbyId);
-        
+
         const lobby: Lobby = {
-            id: lobbyId,
-            name: payload.hostName ?? `${payload.playerId}'s Lobby`,
-            players: [payload.playerId],
-            host: payload.playerId
+            hostId,
+            hostName,
+            lobbyId,
+            lobbyName,
+            settings,
+            players: [hostId],
         };
+
         lobbyIdToLobbyMap.set(lobbyId, lobby);
+        playerToLobbyIdMap.set(hostId, lobbyId);
 
         const update: LobbyUpdate = {
-            lobbyId: lobbyId,
+            lobbyId,
+            lobbyName,
+            hostId,
+            hostName,
             players: lobby.players,
-            hostId: payload.playerId
+            settings,
         };
+
+        console.log([...lobbyIdToLobbyMap.values()]);
 
         io.to(lobbyId).emit('lobbyUpdate', update);
     });
 
-    socket.on('joinLobby', (payload: JoinLobbyEvent) => {
-        let lobby: Lobby | undefined = undefined;
+    socket.on("joinLobby", (payload: JoinLobbyEvent) => {
+        const lobby = lobbyIdToLobbyMap.get(payload.lobbyId);
 
-        if (lobby = lobbyIdToLobbyMap.get(payload.lobbyId)) {
-            socket.join(lobby.id);
-            playerToLobbyIdMap.set(payload.playerId, lobby.id);
-
-            lobby.players.push(payload.playerId);
-
-            const update: LobbyUpdate = {
-                lobbyId: lobby.id,
-                players: lobby.players,
-                hostId: lobby.host
-            };
-
-            io.to(lobby.id).emit('lobbyUpdate', update);
-        } 
-        else {
-            socket.emit("error", {code: 404, message: `Lobby not found with id ${payload.lobbyId}.`});
+        if (!lobby) {
+            socket.emit("error", {
+                code: 404,
+                message: `Lobby not found with id ${payload.lobbyId}.`,
+            });
+            return;
         }
+
+        socket.join(lobby.lobbyId);
+        playerToLobbyIdMap.set(payload.playerId, lobby.lobbyId);
+
+        // prevent duplicate join
+        if (!lobby.players.includes(payload.playerId)) {
+            lobby.players.push(payload.playerId);
+        }
+
+        const update: LobbyUpdate = {
+            lobbyId: lobby.lobbyId,
+            lobbyName: lobby.lobbyName,
+            hostId: lobby.hostId,
+            hostName: lobby.hostName,
+            players: lobby.players,
+            settings: lobby.settings,
+        };
+
+        io.to(lobby.lobbyId).emit("lobbyUpdate", update);
     });
 
-    socket.on('leaveLobby', (payload: LeaveLobbyEvent) => {
-        let lobby: Lobby | undefined = undefined;
+    socket.on("leaveLobby", (payload: LeaveLobbyEvent) => {
+        const lobby = lobbyIdToLobbyMap.get(payload.lobbyId);
 
-        if (lobby = lobbyIdToLobbyMap.get(payload.lobbyId)) {
-            socket.leave(lobby.id);
-            playerToLobbyIdMap.delete(payload.playerId);
-
-            const playerIdx = lobby.players.findIndex(player => player === payload.playerId);
-
-            if (playerIdx > -1) {
-                lobby.players.splice(playerIdx, 1);
-
-                const update: LobbyUpdate = {
-                    lobbyId: lobby.id,
-                    players: lobby.players,
-                    hostId: lobby.host
-                };
-
-                io.to(lobby.id).emit('lobbyUpdate', update);
-            } 
-            else {
-                socket.emit("error", {code: 404, message: `Player ${payload.playerId} not found in Lobby ${payload.lobbyId}.`});
-            }
-        } 
-        else {
-            socket.emit("error", {code: 404, message: `Lobby not found with id ${payload.lobbyId}.`});
+        if (!lobby) {
+            socket.emit("error", {
+                code: 404,
+                message: `Lobby not found with id ${payload.lobbyId}.`,
+            });
+            return;
         }
+
+        socket.leave(lobby.lobbyId);
+        playerToLobbyIdMap.delete(payload.playerId);
+
+        const playerIdx = lobby.players.indexOf(payload.playerId);
+
+        if (playerIdx > -1) {
+            lobby.players.splice(playerIdx, 1);
+        } else {
+            socket.emit("error", {
+                code: 404,
+                message: `Player ${payload.playerId} not found in Lobby ${payload.lobbyId}.`,
+            });
+            return;
+        }
+
+        const update: LobbyUpdate = {
+            lobbyId: lobby.lobbyId,
+            lobbyName: lobby.lobbyName,
+            hostId: lobby.hostId,
+            hostName: lobby.hostName,
+            players: lobby.players,
+            settings: lobby.settings,
+        };
+
+        io.to(lobby.lobbyId).emit("lobbyUpdate", update);
     });
 
     socket.on('startGame', (payload: StartGameEvent) => {
@@ -102,17 +129,17 @@ export function setupSocket(io: Server<ClientToServerEvents, ServerToClientEvent
                 playerId: lobby.players[0]
             };
 
-            io.to(lobby.id).emit('turnStart', update);
-        } 
+            io.to(lobby.lobbyId).emit('turnStart', update);
+        }
         else {
-            socket.emit("error", {code: 404, message: `Lobby not found with id ${payload.lobbyId}.`});
+            socket.emit("error", { code: 404, message: `Lobby not found with id ${payload.lobbyId}.` });
         }
     });
 
     socket.on('chooseWeapon', (payload: ChooseWeaponEvent) => {
         let lobby: Lobby | undefined = undefined;
 
-        if (lobby = lobbyIdToLobbyMap.get(payload.lobbyId)) {    
+        if (lobby = lobbyIdToLobbyMap.get(payload.lobbyId)) {
             // Should be start minigame event once minigame is implemented       
             const update: TurnResolvedEvent = {
                 turnId: payload.turnId,
@@ -123,10 +150,10 @@ export function setupSocket(io: Server<ClientToServerEvents, ServerToClientEvent
                 damage: 5,
             };
 
-            io.to(lobby.id).emit('turnResolved', update);
-        } 
+            io.to(lobby.lobbyId).emit('turnResolved', update);
+        }
         else {
-            socket.emit("error", {code: 404, message: `Lobby not found with id ${payload.lobbyId}.`});
+            socket.emit("error", { code: 404, message: `Lobby not found with id ${payload.lobbyId}.` });
         }
     });
 
@@ -139,10 +166,10 @@ export function setupSocket(io: Server<ClientToServerEvents, ServerToClientEvent
                 playerId: lobby.players[0] == payload.currentPlayer ? lobby.players[1] : lobby.players[0],
             };
 
-            io.to(lobby.id).emit('turnStart', update);
-        } 
+            io.to(lobby.lobbyId).emit('turnStart', update);
+        }
         else {
-            socket.emit("error", {code: 404, message: `Lobby not found with id ${payload.lobbyId}.`});
+            socket.emit("error", { code: 404, message: `Lobby not found with id ${payload.lobbyId}.` });
         }
     });
 }
